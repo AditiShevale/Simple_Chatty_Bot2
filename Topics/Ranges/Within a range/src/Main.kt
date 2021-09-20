5be678dff712c632d32b4e30201a47b8becb92f9
ADITI SHEVALE <aditi.shevale999@gmail.com>
2021-09-20 17:44:14 -0400

    Output true if the fifth number is within at least one range, otherwise output false.

--- Topics/Ranges/Within a range/src/Main.kt
+++ Topics/Ranges/Within a range/src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
-    // write your code here
+
+    val range0 = readLine()!!.toInt()..readLine()!!.toInt()
+    val range1 = readLine()!!.toInt()..readLine()!!.toInt()
+    val num = readLine()!!.toInt()
+    println(num in range0 || num in range1)
+
+
 }
--- Topics/Ranges/Within both ranges/src/Main.kt
+++ Topics/Ranges/Within both ranges/src/Main.kt
@@ -18,11 +18,3 @@ fun main() {
     }
 
 }
-
-//fun main() {
-//    // write your code here
-//    val range0 = readLine()!!.toInt()..readLine()!!.toInt()
-//    val range1 = readLine()!!.toInt()..readLine()!!.toInt()
-//    val num = readLine()!!.toInt()
-//    println(num in range0 && num in range1)
-//}

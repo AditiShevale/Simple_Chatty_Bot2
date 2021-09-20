c31a734a8a1e92bb2f3f3cc647681a560e503e3b
ADITI SHEVALE <aditi.shevale999@gmail.com>
2021-09-20 17:55:53 -0400

    she can have a job only if her age is within 18 to 59 years inclusive

--- Topics/Ranges/Within a range/src/Main.kt
+++ Topics/Ranges/Within a range/src/Main.kt
@@ -1,9 +1,7 @@
 fun main() {
-
     val range0 = readLine()!!.toInt()..readLine()!!.toInt()
     val range1 = readLine()!!.toInt()..readLine()!!.toInt()
     val num = readLine()!!.toInt()
     println(num in range0 || num in range1)
-
 
 }

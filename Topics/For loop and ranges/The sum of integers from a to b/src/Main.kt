470c1a4160f44716223984aeb227a17ec3654668
ADITI SHEVALE <aditi.shevale999@gmail.com>
2021-09-20 18:41:49 -0400

    Read two integers a and b. Print the sum of all the integers from a tob inclusive. It is guaranteed that a <b.

---
+++ Topics/For loop and ranges/The sum of integers from a to b/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val a = readLine()!!.toInt()
+    val b = readLine()!!.toInt()
+    var result = 0
+    for (i in a..b){
+        result +=i
+    }
+    println(result)
+}
--- Topics/For loop and ranges/Theory/src/Main.kt
+++ Topics/For loop and ranges/Theory/src/Main.kt
@@ -1,9 +1,5 @@
 // You can experiment here, it won’t be checked
 
 fun main(args: Array<String>) {
-    for (i in 1..3) {
-        for (j in 1..i) {
-            print(j)
-        }
-    }
+  // put your code here
 }

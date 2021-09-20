3811abb88b0ca29c97aa76eb45787470080d85ba
ADITI SHEVALE <aditi.shevale999@gmail.com>
2021-09-20 16:55:14 -0400

    Stage 3/5: Guess the age

--- Simple Chatty Bot/task/src/bot/SimpleBot.kt
+++ Simple Chatty Bot/task/src/bot/SimpleBot.kt
@@ -10,7 +10,17 @@ fun main() {
     println("I was created in 2020.")
     println("Please, remind me your name.")
 
-    val yourName = readLine()
+    val name = scanner.nextLine()
 
-    println("What a great name you have, ${yourName}!")
+    println("What a great name you have, $name!")
+    println("Let me guess your age.")
+    println("Enter remainders of dividing your age by 3, 5 and 7.")
+
+    // reading all remainders
+
+    val remainder3 = readLine()!!.toInt()
+    val remainder5 = readLine()!!.toInt()
+    val remainder7 = readLine()!!.toInt()
+    val yourAge = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105
+    println("Your age is ${yourAge}; that's a good time to start programming!")
 }
